127c8ab572ac0cf5637fb8a0d73cc914c380339d
thanhloansqt <thanhloansqt@gmail.com>
2015-12-07 22:44:41 +0700

    edit form

--- contact/forms.py
+++ contact/forms.py
@@ -8,16 +8,24 @@ class ContactForm(forms.ModelForm):
         model = Contact
         fields = ["full_name", 'email', 'phone', 'title', 'message']
 
-    # def clean_email(self):
-    #     print(self.cleaned_data)
-    #     email = self.cleaned_data.get('email')
-    #     email_base, provider = email.split("@")
-    #     domain, extension = provider.split('.')
-    #     if not extension == "edu":
-    #         raise forms.ValidationError("Please use a valid email!")
-    #     return(email)
+    def clean_email(self):
+        print(self.cleaned_data)
+        email = self.cleaned_data.get('email')
+        email_base, provider = email.split("@")
+        domain, extension = provider.split('.')
+        if not extension == "edu":
+            raise forms.ValidationError("Please use a valid email!")
+        return(email)
     #
     # def clean_phone(self):
     #     print(self.cleaned_data)
     #
     #     return("111")
+
+    def clean_message(self):
+        message = self.cleaned_data.get('message')
+        print("zzzzzz"+message)
+        if not message:
+          return "NOT"
+        else:
+          return "OK"
