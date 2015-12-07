029ba1e3024942ac752a91ad3bf9b9da35c85079
ducthinhdt <ducthinhdt@gmail.com>
2015-12-07 18:40:57 +0700

    change email template

--- contact/views.py
+++ contact/views.py
@@ -31,15 +31,33 @@ def home(request):
             print(instance.phone)
             # print(instance.title)
             print(instance.message)
+            # mail = EmailMultiAlternatives(
+				# subject="[PhanThietFood] Contact | %s - %s" % (instance.full_name, instance.title),
+    			# body=instance.message,
+	 		# 	from_email="%s <ptf@phanthietfood.com>" % (instance.full_name),
+            #     # from_email="Yamil Asusta <ptf@phanthietfood.com>",
+ 			# 	to=["ptf@phanthietfood.com"],
+			 #    headers={"Reply-To": "ptf@phanthietfood.com"}
+            # )
+            # # mail.attach_alternative("<p>This is a simple HTML email body</p>", "text/html")
+            mail_subject="[PhanThietFood] Contact | %s - %s" % (instance.full_name, instance.title)
+            mail_body="Full name: " + instance.full_name + "<br>" + \
+                        "Email: " + instance.email +  "<br>"  + \
+                        "Phone: " + instance.phone +  "<br>"  + \
+                        "Title: " + instance.title +  "<br>"  + \
+                        "Message: " + instance.message
+            mail_from_email="%s <ptf@phanthietfood.com>" % (instance.full_name)
+            mail_to=["ptf@phanthietfood.com"]
+            mail_headers={"Reply-To": "ptf@phanthietfood.com"}
             mail = EmailMultiAlternatives(
-				subject="[PhanThietFood] Contact - %s" % (instance.full_name),
-    			body=instance.message,
-	 			from_email="%s <ptf@phanthietfood.com>" % (instance.full_name),
+				subject=mail_subject,
+    			body=mail_body,
+	 			from_email=mail_from_email,
                 # from_email="Yamil Asusta <ptf@phanthietfood.com>",
- 				to=["ptf@phanthietfood.com"],
-			    headers={"Reply-To": "ptf@phanthietfood.com"}
+ 				to=mail_to,
+			    headers=mail_headers
             )
-            # mail.attach_alternative("<p>This is a simple HTML email body</p>", "text/html")
+            mail.attach_alternative(mail_body, "text/html")
             mail.send()
         except:
             print("Unexpected error:", sys.exc_info()[0])
